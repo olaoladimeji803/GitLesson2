bf58cea781733e169dc305ac67561fc266843f1e
OIyiola <97890183+OIyiola@users.noreply.github.com>
2024-08-25 23:46:41 +0100

    Forth commit

--- GitEnv.js
+++ GitEnv.js
@@ -1,3 +1,3 @@
-console.log("GitTraining Version2.2 update");
-console.log("GitTraining in progress Version2.2");
-console.log("GitTraining in progress and update Version2.2");
+console.log("GitTraining Version2.5 update");
+console.log("GitTraining in progress Version2.5");
+console.log("GitTraining in progress and update Version2.5");
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 console.log('index.js');
-console.log('version2.2 update update');
-console.log('version2.2 update in progress');
+console.log('version2.5 update update');
+console.log('version2.5 update in progress');
